681c4abde3de9f93fbd6b95c7ee52a45f500f69e
Andreza Feu <andrezafeu@gmail.com>
2016-04-27 11:40:27 -0400

    Prompt current location or error message

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 if ("geolocation" in navigator) {
 	console.log("Geolocation is available");
+	navigator.geolocation.getCurrentPosition(displayPosition, showPositionError);
 } else {
 	alert("You don't have Geolocation. Time to upgrade your browser.");
+}
+
+function displayPosition (data) {
+	console.log("Got position!")
+	console.log(data)
+}
+
+function showPositionError (error) {
+	console.log("Failed to get position :(")
+	console.log(error)
 }
